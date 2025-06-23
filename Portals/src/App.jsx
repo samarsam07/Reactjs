7849a3e9cacff9de5fc16e326e9ca036fde52691
samar <samarmohd251@gmail.com>
2025-06-23 20:37:09 +0530

    refactor: remove unused context providers and integrate UseReducer component

--- Portals/src/App.jsx
+++ Portals/src/App.jsx
@@ -1,18 +1,13 @@
-import React, { createContext } from "react";
-import ComponentA from "./components/ComponentA";
-export const Data = createContext();
-export const Data1 = createContext();
+import React from "react";
+import UseReducer from "./components/UseReducer";
+
 
 const App = () => {
   const name = "John Doe";
   const age = 30;
   return (
     <div>
-      <Data.Provider value={name}>
-        <Data1.Provider value={age}>
-          <ComponentA />
-        </Data1.Provider>
-      </Data.Provider>
+      <UseReducer />
     </div>
   );
 };
